18ab5ef120e3a624abb8ee33355a1ca5db324b3d
gmendes25 <gerson-hp1@hotmail.com>
2022-08-24 10:22:37 -0300

    Complete_Produto_Response

--- src/main/java/com/example/demo/View/model/ProdutoResponse.java
+++ src/main/java/com/example/demo/View/model/ProdutoResponse.java
@@ -2,4 +2,48 @@ package com.example.demo.View.model;
 
 public class ProdutoResponse {
     
+    private Integer id;
+    
+    private String name;
+
+    private Integer quantity;
+
+    private Double value;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public ProdutoResponse() {
+    }
+
+    
 }
